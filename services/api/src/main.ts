157ae3e3875665ee964e2e4266691f67e8d1ba76
Ayo Richard ABE [gODtECH] <ayoabe24@gmail.com>
2026-09-04 12:13:16 +0100

    fix: use explicit ESM extension in API entrypoint

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -3,7 +3,7 @@ import { ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
-import { AppModule } from "./app.module";
+import { AppModule } from "./app.module.js";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
